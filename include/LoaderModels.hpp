9b58b574fecfa67c9600fbd175d2e85ab790b2b1
Tausif ali <ajaxhis@tutanota.com>
2020-06-24 23:34:11 +0530

    added models for the resources that will be loaded

---
+++ include/LoaderModels.hpp
@@ -0,0 +1,30 @@
+
+
+#ifndef LOADER_MODELS_HPP
+#define LOADER_MODELS_HPP
+
+#include <vector>
+#include <string>
+
+#include <SFML/Graphics/Rect.hpp>
+
+struct TextureModel
+{
+  int handle;
+  std::string path;
+};
+struct AnimationModel
+{
+  int handle;
+  int texHandle;
+  int frameCount;
+
+  std::vector<const sf::IntRect> frames;
+};
+struct LoaderModel
+{
+  int handle;
+  std::string path;
+};
+
+#endif
