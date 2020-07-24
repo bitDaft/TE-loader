0f3d0a343b436386a5a7ac90434ea59976998f58
Tausif ali <anodecode@gmail.com>
2020-07-24 05:59:36 +0530

    basic loader for testing

--- src/main.cpp
+++ src/main.cpp
@@ -5,47 +5,59 @@
 
 int main(int argc, char const *argv[])
 {
-  // Loader *ldr = new Loader("test.teldr");
-  // std::cout << "\nprinting files";
-  // std::cout << "\nTextures:\n";
-  // if (ldr->flags < 0)
-  // {
-  //   std::cout << "An error occured while reading file";
-  // }
-  // else
-  // {
-  //   for (auto &i : ldr->getTexture())
-  //   {
-  //     std::cout << i->handle << " ";
-  //     std::cout << i->path;
-  //     std::cout << "\n";
-  //   }
-  //   std::cout << "\nAnimations:\n";
-  //   for (auto &i : ldr->getAnimation())
-  //   {
-  //     std::cout << i->handle << " ";
-  //     std::cout << i->texHandle;
-  //     std::cout << "\n";
-  //     for (auto &j : i->frames)
-  //     {
-  //       std::cout << j->top << " ";
-  //       std::cout << j->left << " ";
-  //       std::cout << j->width << " ";
-  //       std::cout << j->height << " ";
-  //       std::cout << "\n";
-  //     }
-  //     std::cout << "\n";
-  //   }
-  //   std::cout << "\nLoaders:\n";
-  //   for (auto &i : ldr->getLoader())
-  //   {
-  //     std::cout << i->handle << " ";
-  //     std::cout << i->path;
-  //     std::cout << "\n";
-  //   }
-  // }
+  Loader *ldr = new Loader("basic.teldr");
+  if (ldr->flags < 0)
+  {
+    std::cout << "An error occured while reading file";
+  }
+  else
+  {
+    std::cout << "\nprinting files\n";
+    std::cout << "\nTextures:\n";
+    for (auto &i : ldr->getTexture())
+    {
+      std::cout << i->handle << " ";
+      std::cout << i->path;
+      std::cout << "\n";
+    }
+    std::cout << "\nAnimations:\n";
+    for (auto &i : ldr->getAnimation())
+    {
+      std::cout << i->handle << " ";
+      std::cout << i->texHandle << " ";
+      std::cout << i->tileBased;
+      std::cout << "\n";
+      if (i->tileBased)
+      {
+        for (auto &j : i->tileids)
+        {
+          std::cout << j << " ";
+          std::cout << "\n";
+        }
+      }
+      else
+      {
+        for (auto &j : i->frames)
+        {
+          std::cout << j->top << " ";
+          std::cout << j->left << " ";
+          std::cout << j->width << " ";
+          std::cout << j->height << " ";
+          std::cout << "\n";
+        }
+      }
+      std::cout << "\n";
+    }
+    std::cout << "\nLoaders:\n";
+    for (auto &i : ldr->getLoader())
+    {
+      std::cout << i->handle << " ";
+      std::cout << i->path;
+      std::cout << "\n";
+    }
+  }
 
-  Loader *ldr = new Loader();
+  // Loader *ldr = new Loader();
   // AnimationModel *anim = new AnimationModel();
   // anim->handle = 1;
   // anim->texHandle = 1;
@@ -59,11 +71,12 @@ int main(int argc, char const *argv[])
   // tex->handle = 1;
   // tex->path = "./assets/sheet2.png";
   // ldr->addTexture(tex);
-  LoaderModel *loader = new LoaderModel();
-  loader->handle = 1;
-  loader->path = "./basic.teldr";
+  // LoaderModel *loader = new LoaderModel();
+  // loader->handle = 1;
+  // loader->path = "./basic.teldr";
+  // ldr->addLoader(loader);
 
-  ldr->saveToFile("boot");
+  // ldr->saveToFile("basic");
 
   return 0;
 }
